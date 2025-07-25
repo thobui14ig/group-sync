84ff469bf7fd702e1abc2a065062607405ac9648
thobui14ig <buithanhtho14ig@gmail.com>
2025-07-25 20:21:40 +0700

    ok

--- src/application/sync-new-post-group/sync-new-post-group.usecase.ts
+++ src/application/sync-new-post-group/sync-new-post-group.usecase.ts
@@ -7,7 +7,7 @@ import { delay, getHttpAgent } from 'src/common/helper';
 import { ProxyEntity, ProxyStatus } from 'src/infrastructure/entity/proxy.entity';
 import { GroupRepository } from 'src/infrastructure/repository/user/group.repository';
 import { ProxyRepository } from 'src/infrastructure/repository/user/proxy.repository';
-import { AppGateway } from 'src/infrastructure/socket/app.gateway';
+import { AppGateway, Message } from 'src/infrastructure/socket/app.gateway';
 import { faker } from '@faker-js/faker';
 import { firstValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
@@ -63,97 +63,45 @@ export class SyncNewPostGroupUseCase {
                 // if (!proxy) continue;
                 const httpsAgent = getHttpAgent(this.proxy)
 
-                await fetch(`https://www.facebook.com/groups/${groupId}/?sorting_setting=CHRONOLOGICAL`, {
+                await fetch(`https://graph.facebook.com/${groupId}/feed?limit=10&&fields=id,created_time,updated_time,message&access_token=EAADo1TDZCuu8BPP9VEMs0kU3Ux9ZB8ZC9hJdlkLzcLbjkUy2IUZCxHcDzoq1OYNt7bZBWWSghjuZAD0DMVgNCkuS1MgISS3Y78ZC9Nu66BuzoPuoSnKD5hsRgkLUyX8sbp6PebwhEvZAm4mZBVEv4eFVSvRBarsv2Rcq68K5Lq7PvERFkwZAXleKQlWmwrg2OZCue04FwZDZD`, {
                     "headers": {
                         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                         "accept-language": "vi,en-US;q=0.9,en;q=0.8",
                         "cache-control": "max-age=0",
-                        "dpr": "1.1",
                         "priority": "u=0, i",
-                        "sec-ch-prefers-color-scheme": "light",
                         "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"",
-                        "sec-ch-ua-full-version-list": "\"Not)A;Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"138.0.7204.158\", \"Google Chrome\";v=\"138.0.7204.158\"",
                         "sec-ch-ua-mobile": "?0",
-                        "sec-ch-ua-model": "\"\"",
                         "sec-ch-ua-platform": "\"Windows\"",
-                        "sec-ch-ua-platform-version": "\"10.0.0\"",
                         "sec-fetch-dest": "document",
                         "sec-fetch-mode": "navigate",
-                        "sec-fetch-site": "same-origin",
+                        "sec-fetch-site": "none",
                         "sec-fetch-user": "?1",
                         "upgrade-insecure-requests": "1",
-                        "viewport-width": "860",
-                        "cookie": "sb=IpN2Z63pdgaswLIv6HwTPQe2; ps_l=1; ps_n=1; vpd=v1%3B896x414x2; datr=VbQ5aIRhuL3sZgH-YD17GNjn; wl_cbv=v2%3Bclient_version%3A2856%3Btimestamp%3A1751184877; locale=vi_VN; dpr=1.100000023841858; c_user=100018203168307; xs=15%3AXaSKw8QtKSgtmQ%3A2%3A1753401542%3A-1%3A-1; i_user=61578848633136; fr=10GZ0B4TTAurKHsYO.AWf2LH7zGppwCKeC_3Tr3-KcwqyXLTgI17gDh6LU_SutrOS5O54.BogPLm..AAA.0.0.BogsjN.AWcUaFGEIo8zdOWmcBZ5l6bfmGk; wd=860x866",
+                        "cookie": "sb=IpN2Z63pdgaswLIv6HwTPQe2; ps_l=1; ps_n=1; vpd=v1%3B896x414x2; datr=VbQ5aIRhuL3sZgH-YD17GNjn; wl_cbv=v2%3Bclient_version%3A2856%3Btimestamp%3A1751184877; locale=vi_VN; dpr=1.100000023841858; wd=1745x866; c_user=100018203168307; xs=32%3AJYyqJ1egcAyDeg%3A2%3A1753447955%3A-1%3A-1; fr=1glvfPv6RwbgxwWrq.AWd9iOFb4R207-8h6IhsDkL6MP_qpJ0HkZdS32EuHERIU_njNhw.Bog3hy..AAA.0.0.Bog34U.AWc8BFBOwCiJSsrkS2Iy3p9q2QY; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1753448286433%2C%22v%22%3A1%7D"
                     },
                     "body": null,
                     "method": "GET"
                 })
                     .then(async (response) => {
                         const text = await response.text();
-                        const p = this.getPostId(text)
-                        const m = this.getMessage(text)
-                        let i = this.getUserId(text)
-                        let n = this.getName(text)
-
-                        if (!m || m?.length === 0) return
-                        if ((!n || n?.length === 0)) {
-                            n = "Người tham gia ẩn danh"
-                            i = null;
+                        const latestPost = JSON.parse(text)?.data?.reduce((latest, post) => {
+                            return new Date(post.created_time) > new Date(latest.created_time) ? post : latest;
+                        });
+                        const message: Message = {
+                            actorId: "1cf35b25-d816-408a-9ef0-81448b1d7653",
+                            actorName: 'No Name',
+                            postId: latestPost?.id?.split('_')[1],
+                            content: latestPost?.message,
+                            createdAt: dayjs(latestPost?.created_time).utc().format('YYYY-MM-DD HH:mm:ss'),
+                            groupId
                         }
-                        const isRemove = this.remove(m)
+                        const isRemove = this.remove(message.content)
 
                         if (isRemove) {
                             return
                         }
-
-                        const message = {
-                            actorId: i,
-                            actorName: n,
-                            postId: p,
-                            content: m,
-                            createdAt: dayjs().utc().format('YYYY-MM-DD HH:mm:ss'),
-                            groupId
-                        }
                         console.log("🚀 ~ SyncNewPostGroupUseCase ~ .then ~ message:", message)
                         return this.gateway.receiveMessage(message)
-
-                        // if (typeof text === 'string') {
-                        //     const lines = text.trim().split('\n');
-                        //     const dataJson = JSON.parse(lines[0])
-                        //     const responsePost = dataJson?.data?.node?.group_feed?.edges[0].node
-                        //     const actorId = responsePost?.["actors"]?.[0]?.["id"]
-                        //     const actorName = responsePost?.["actors"]?.[0]?.["name"]
-                        //     const postId = responsePost?.["post_id"]
-                        //     const content = responsePost?.["comet_sections"]?.["content"]?.["story"]?.["comet_sections"]?.["message"]?.["story"]?.["message"]?.["text"]
-                        //     const createdAt = dayjs.unix(responsePost?.["comet_sections"]?.["timestamp"]?.["story"]?.["creation_time"]).utc().format('YYYY-MM-DD HH:mm:ss');
-
-                        //     // if (dataJson?.errors?.[0]?.code === 1675004 && this.proxy.id !== -1) {
-                        //     //     this.proxy.status = ProxyStatus.IN_ACTIVE
-                        //     //     return this.proxyRepository.update(this.proxy)
-                        //     // }
-                        //     const message = {
-                        //         actorId,
-                        //         actorName,
-                        //         postId,
-                        //         content,
-                        //         createdAt,
-                        //         groupId
-                        //     }
-                        //     const isRemove = this.remove(message.content)
-
-                        //     if (isRemove) {
-                        //         return
-                        //     }
-                        //     console.log("🚀 ~ SyncNewPostGroupUseCase ~ .then ~ message:", message)
-
-                        //     if (actorId && actorName && postId && content) {
-                        //         const key = `${groupId}_${createdAt.replaceAll("-", "").replaceAll(" ", "").replaceAll(":", "")}`
-                        //         const isExistKey = await this.redisService.checkAndUpdateKey(key)
-                        //         if (!isExistKey) {
-                        //             return this.gateway.receiveMessage(message)
-                        //         }
-                        //     }
-                        // }
                     })
                     .catch(async (error) => {
                         console.log("🚀 ~ SyncNewPostGroupUseCase ~ crawlPublic ~ error:", error?.message)
@@ -262,21 +210,16 @@ export class SyncNewPostGroupUseCase {
                     'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
                 ];
 
-                const userAgent = faker.internet.userAgent()
-                const apceptLanguage = languages[Math.floor(Math.random() * languages.length)]
-
-
-
                 const res = await firstValueFrom(
-                    this.httpService.get(`https://graph.facebook.com/1671812873703572/feed?limit=10&&fields=id,created_time,updated_time,message&access_token=EAABsbCS1iHgBPBZA83BVFwzyNf2PqS2vOloPy4d5cBNiw4vXY2K0oZBZCKswCjtTuAGk9UyNOxdw6UrLZB4mtpIMFvJKahIbJh08DfrcAl9qAJqZCXZC6xvbLXmDMvecXzVbY17JeXyDeAZCZAZCP54ZAZBTBX7zrRJja7P9BVdyk27UeE7Je1E8Vl5RwrARGQqhjslUAZDZD`, {
+                    this.httpService.get(`https://graph.facebook.com/${groupId}/feed?limit=10&&fields=id,created_time,updated_time,message&access_token=EAADo1TDZCuu8BPP9VEMs0kU3Ux9ZB8ZC9hJdlkLzcLbjkUy2IUZCxHcDzoq1OYNt7bZBWWSghjuZAD0DMVgNCkuS1MgISS3Y78ZC9Nu66BuzoPuoSnKD5hsRgkLUyX8sbp6PebwhEvZAm4mZBVEv4eFVSvRBarsv2Rcq68K5Lq7PvERFkwZAXleKQlWmwrg2OZCue04FwZDZD`, {
                         // httpsAgent,
                         // params
                     }),
                 );
-                // const latestPost = res.data.data.reduce((latest, post) => {
-                //     return new Date(post.created_time) > new Date(latest.created_time) ? post : latest;
-                // });
-                console.log("🚀 ~ SyncNewPostGroupUseCase ~ crawlPrivate ~ res:", res.data.data[0])
+                const latestPost = res.data.data.reduce((latest, post) => {
+                    return new Date(post.created_time) > new Date(latest.created_time) ? post : latest;
+                });
+                console.log("🚀 ~ SyncNewPostGroupUseCase ~ crawlPrivate ~ res:", latestPost)
 
             } catch (error) {
                 console.log("🚀 ~ SyncNewPostGroupUseCase ~ crawlPrivate ~ error:", error?.response?.data?.error)
